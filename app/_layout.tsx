85a4585508324943997861a7d7003282e0801c17
Caio Alfonso <caioap25@gmail.com>
2024-10-30 07:22:23 -0300

    Added theme provider to the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,24 @@
 import { Stack } from "expo-router";
+import {
+  MD3LightTheme as DefaultTheme,
+  PaperProvider,
+} from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#ff7733",
+  },
+};
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <PaperProvider theme={theme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </PaperProvider>
   );
 }
