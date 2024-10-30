f0535fe4038b8a30ae8a5228f5b32d7597bcefd1
Caio Alfonso <caioap25@gmail.com>
2024-10-30 07:21:34 -0300

    Created new global component CurrencyField

---
+++ components/CurrencyField.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { KeyboardTypeOptions, StyleSheet } from "react-native";
+import { TextInput } from "react-native-paper";
+
+interface Props {
+  label?: string;
+  placeholder?: string;
+  initValue: number | undefined;
+  maxLength?: number;
+  onChange: (currency: number) => void;
+}
+
+const formatToCurrency = (amount: number): string => {
+  amount = amount / 100;
+  return Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(amount);
+};
+
+const parseCurrencyToNumber = (currency: string): number => {
+  const [integerPart, decimalPart] = currency.split(",");
+  return Number(`${integerPart.replace(/[^0-9]/g, "")}.${decimalPart || 0}`);
+};
+
+const maskCurrency = (input: string): string => {
+  const cleanedInput = Number(input.replace(/[^0-9]/g, ""));
+  const paddedInput = cleanedInput.toString().padStart(3, "0");
+  const integerPart = paddedInput
+    .slice(0, -2)
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  const decimalPart = paddedInput.slice(-2);
+  return `R$ ${integerPart},${decimalPart}`;
+};
+
+export default function CurrencyField({
+  label,
+  placeholder,
+  initValue,
+  maxLength = 18,
+  onChange,
+}: Props) {
+  const [value, setValue] = useState(formatToCurrency(initValue || 0));
+
+  const onChangeText = (text: string) => {
+    if (maxLength !== undefined && text.length >= maxLength) return;
+    text = maskCurrency(text);
+    setValue(text);
+    onChange(parseCurrencyToNumber(text));
+  };
+
+  return (
+    <TextInput
+      mode="outlined"
+      outlineColor="#aaaaaa"
+      placeholderTextColor="#aaaaaa"
+      keyboardType="numeric"
+      label={label}
+      placeholder={placeholder}
+      value={value}
+      style={styles.input}
+      outlineStyle={styles.inputWrapper}
+      onChangeText={onChangeText}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  inputWrapper: {
+    borderRadius: 8,
+  },
+  input: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+});
